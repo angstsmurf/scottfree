e23b2a458b3c76d9f32bddcabd74ae63e77bc633
angstsmurf <ignalina@me.com>
2022-02-14 11:39:18 +0100

    Initial Commit

---
+++ scottfree/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  scottfree
+//
+//  Created by Administrator on 2022-02-14.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
